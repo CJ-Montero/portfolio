9f2f0042ace246bbd5bcb3bb73473899e9be8247
CJ Montero <cristlad@proton.me>
2025-07-05 20:22:57 -0400

    Añadidos archivos para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const LIVE_URL = 'https://CJ-Montero.github.io';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://CJ-Montero.github.io',
+  base: '/',
 });
