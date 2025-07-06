41e6e7247bd4491ff2a4de74667a10f0aaeb2477
CJ Montero <cristlad@proton.me>
2025-07-06 10:21:21 -0400

    Paquetes para Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   site: 'https://cj-montero.github.io',
   base: 'cj-montero.github.io',
+
   vite: {
     plugins: [tailwindcss()]
-  }
-});
+  },
 
+  adapter: netlify()
+});
