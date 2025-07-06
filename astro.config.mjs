14c20c1659172e5f2df4d387295653770d7e63a6
CJ Montero <cristlad@proton.me>
2025-07-05 21:02:50 -0400

    cambios en paquetes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-
 export default defineConfig({
-  build: {
-    assetsPrefix: './',
-  },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  base: '/',
   site: 'https://cj-montero.github.io',
-  trailingSlash: 'never', // opcional
+  base: '/',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
+
